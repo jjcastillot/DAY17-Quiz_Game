e8edba3a78a633759475e41bae5bf6038d168951
jjcastillot <jjcastillot@gmail.com>
2022-12-02 18:57:57 -0500

    Defined classes and main script

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,1 +1,9 @@
+class QuizBrain:
 
+    def __init__(self,question):
+        self.question_number = 0
+        self.question_list = question
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q.{self.question_number+1}: {current_question.text} (True/False) ")
