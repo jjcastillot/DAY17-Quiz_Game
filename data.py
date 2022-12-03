f2e8502758e0ffa535fc79658f7f4edb95b5a8e1
jjcastillot <jjcastillot@gmail.com>
2022-12-03 07:56:52 -0500

    PROJECT FINISHED. Updated question bank.

--- data.py
+++ data.py
@@ -1,14 +1,18 @@
-question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+question_data = [{"category": "Animals", "type": "boolean", "difficulty": "medium", "question": "An octopus can fit through any hole larger than its beak.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Entertainment: Music", "type": "boolean", "difficulty": "medium", "question": "Soulja Boy&#039;s &#039;Crank That&#039; won a Grammy for Best Rap Song in 2007.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "History", "type": "boolean", "difficulty": "medium", "question": "The two atomic bombs dropped on Japan by the United States in August 1945 were named &#039;Little Man&#039; and &#039;Fat Boy&#039;.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Vehicles", "type": "boolean", "difficulty": "medium", "question": "The Japanese Shinkansen beat the French TGV&#039;s speed record for fastest electric rail train.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium", "question": "The first Maxis game to feature the fictional language &quot;Simlish&quot; was The Sims (2000).", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Science & Nature", "type": "boolean", "difficulty": "medium", "question": "Type 1 diabetes is a result of the liver working improperly.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The term &quot;Spam&quot; came before the food product &quot;Spam&quot;.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Sports", "type": "boolean", "difficulty": "medium", "question": "The Olympics tennis court is a giant green screen.", "correct_answer": "True", "incorrect_answers": [
+    "False"]}, {"category": "Geography", "type": "boolean", "difficulty": "medium", "question": "Japan has left-hand side traffic.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "History", "type": "boolean", "difficulty": "medium", "question": "The Hundred Years&#039; War was fought for more than a hundred years.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium", "question": "Pistons were added to Minecraft in Beta 1.5.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium", "question": "In the video game &quot;Transistor&quot;, &quot;Red&quot; is the name of the main character.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium", "question": "The character that would eventually become Dr. Eggman was considered for the role of Sega&#039;s new flagship mascot before Sonic was.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "History", "type": "boolean", "difficulty": "medium", "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.", "correct_answer": "False", "incorrect_answers": ["True"]}]
+
+
+# [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
