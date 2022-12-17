0033e303a8d0c30c288099352eeabd30bbe4d4f9
jjcastillot <jjcastillot@gmail.com>
2022-12-17 07:07:09 -0500

    Test commit.

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the quiz")
-print("###Your final score was {quiz.score}/{quiz.question_number}.###")
+print("####Your final score was {quiz.score}/{quiz.question_number}.####")
